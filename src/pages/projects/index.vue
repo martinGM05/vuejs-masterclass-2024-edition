118f08c6c563f6dc631012735915c1d1a7b0488d
Martin Gonzalez Miranda <martin.gonzalez@cyberpuerta.mx>
2025-01-21 14:32:44 -0600

    feat: a lot of changes, mainly add unplugin to route in base folders

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import { ref } from 'vue';
+
+const projects: any = ref(null);
+
+(async () => {
+  const { data, error } = await supabase.from('projects').select();
+
+  if (error) console.error(error);
+
+  projects.value = data;
+  console.log('Projects:', projects);
+})();
+
+</script>
+
+<template>
+    <div>
+      <h1>Projects page</h1>
+      <RouterLink to="/">Go to home</RouterLink>
+      <pre>{{ projects }}</pre>
+    </div>
+</template>
